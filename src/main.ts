2fbd35e9f15ffa7baffa37dd8b68f96964358552
Jeremy <jrvtexas@outlook.com>
2023-10-09 08:49:34 -0500

    initial
    initial push

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+// import './style.css'
+// import typescriptLogo from './typescript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.ts'
+
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <!-- add some html to the #app element in index.html -->
+// `
+
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
